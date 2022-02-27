3f573d4ee0c053550dbd06ec068c74095be86de4
webseb00 <lordow91@gmail.com>
2022-02-27 22:23:06 +0100

    Create About section

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,8 @@ module.exports = {
     options: {
       "projectId": "wsccgrx8",
       "dataset": "production",
-      watchMode: true
+      watchMode: true,
+      'token': 'sk81aPTZm7Ex2mcB4Y6wJQcQvewvkwJAcdVKACgFyIc5tdP3nYUrwfcHPfA1P6qOvX44cCMHceyb0z9X7b2sUD5ksq9h4TiTKDkqSnbRuSp3bG6jNMz6Vg5mKnz2CiHRzbLQgq9hVl50d2OmXLa1ejLkbZ0wFbxm8qQGsB3nYl2ORewR6PAh'
     }
   }, "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
--- src/container/About/About.js
+++ src/container/About/About.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { images } from '../../constants/index';
+import { useStaticQuery, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import './About.scss';
 
 const About = () => {
+
+  const data = useStaticQuery(graphql`
+  query AboutsQuery {
+    allSanityAbouts {
+      nodes {
+        description
+        title
+        imgUrl {
+          asset {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }  
+  `);
+  
+  const { nodes } = data.allSanityAbouts;
+
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text">
+        I know that <span>Good Design</span><br /> means <span>Good Business</span>
+      </h2>
+      <div className="app__profiles">
+        {nodes.map((item, index) => {
+          const image = getImage(item.imgUrl.asset);
+          return (
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+              className="app__profile-item"
+              key={`${item.title + index}`}
+            >
+              <GatsbyImage image={image} alt={item.title} />
+              <h2 className="bold-text" style={{ marginTop: 20 }}>{item.title}</h2>
+              <p className="p-text" style={{ marginTop: 10 }}>{item.description}</p>
+            </motion.div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
